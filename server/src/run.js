851f8fd2c588c24d79d31bd51acaa64f06084a3d
Rohit <rohit.rthd.04@gmail.com>
2025-10-06 18:20:10 +0530

    adding cors for frontend for the compatibility process

--- server/src/run.js
+++ server/src/run.js
@@ -7,7 +7,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+
+const corsOptions = ({
+  origin: process.env.VITE_CLIENT_URL,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+})
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/employees", employeeRoutes);
